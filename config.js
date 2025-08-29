a711149df5909c72bbc3d2ae530215df0ecb456b
bro-panel <imdbro7@gmail.com>
2025-08-29 15:24:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "kvlCGA7B#W0dEyiwD2jYYLxETAptBnL4b7M2DHb_ulYCXZmVPRxE", // session id
+SESSION_ID: process.env.SESSION_ID || "S=X0JnQR5I#irYSW2SHGX6G6hin9POcovaLX_DbJHU5IF_N7UfE6Fk", // session id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", // true,false
 ANTI_DELETE: process.env.ANTI_DELETE || "true", // true,false
 MODE: process.env.MODE || "public", // public,private,groups,inbox
